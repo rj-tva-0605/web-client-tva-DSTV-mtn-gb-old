abdebf80294409bd0a3c93ad734b8bead2b5258f
RJ-0605 <rojedkay@gmail.com>
2021-10-21 12:49:19 +0000

    changes submitted for the package buying to axios have to change host from localhost for payment to be successful

--- src/pages/PackagesPage/modals/Purchasemodal.jsx
+++ src/pages/PackagesPage/modals/Purchasemodal.jsx
@@ -46,10 +46,35 @@ const Purchasemodal = ({showPayAlert, setShowPayAlert,
     });
     
 
+    
     const proceedfunc = (e) => {
       e.preventDefault();
 
       // dispatch(loginReducer(data))
+      
+      var data = JSON.stringify({
+        "mobilenumber": "966601969",
+        "productid": "24501220000005180"
+      });
+
+      var config = {
+        method: 'post',
+        url: 'https://tvanywheretest-ott.magnaquest.com/MTNGBSDPPG/subscription',
+        headers: { 
+          'Authorization': 'Basic TVROR0JVQ1dFQlVTUjpNdG5nYnBhc3NAMTIzNA==', 
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+
+      axios(config)
+      .then( response => {
+        console.log("Pay response for Package", response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
       history.push({
         pathname:  "/dashboard",
         
